2527208c65ecd1f62c58c3689c6698114cdb1e29
jessie-2023 <jessie.xie2023@gmail.com>
2025-01-21 11:35:06 +0000

    smiley face with React

--- viteReact/src/main.tsx
+++ viteReact/src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 10;
+const eyeOffsetX = 90;
+const eyeOffsetY = 90;
+const eyeRadius = 50;
+
+const App = () => (
+    <svg width={width} height={height}>
+      <circle
+        cx={centerX}
+        cy={centerY}
+        r={centerY - strokeWidth / 2}
+        fill='yellow'
+        stroke='black'
+        stroke-width={strokeWidth}
+      />
+      <circle 
+            cx={centerX - eyeOffsetX} 
+            cy={centerY - eyeOffsetY}  
+            r={eyeRadius}            
+        >
+        </circle>
+        <circle 
+            cx={centerX + eyeOffsetX} 
+            cy={centerY - eyeOffsetY}  
+            r={eyeRadius}            
+        ></circle>
+    </svg>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
